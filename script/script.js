f57651ab9e8875edf9781823f11958ce9d372ab9
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2023-11-04 23:52:07 -0300

    Style: Improvement of about page

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const carousel = document.getElementById("carousel");
+
+let isDragging = false;
+
+const dragStart = () =>{
+    isDragging = true;
+    carousel.classList.add("dragging");
+}
+
+const dragging = (e) =>{
+    if (!isDragging) return;
+    carousel.scrollLeft = e.pageX
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
