5e90e6476a530e24861b6f34bede5295a41c1d68
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2023-11-05 04:16:37 -0300

    Style: finished stylization about page

--- script/script.js
+++ script/script.js
@@ -1,16 +1,24 @@
 const carousel = document.getElementById("carousel");
 
-let isDragging = false;
+let isDragging = false, startX, startScrollLeft;
 
-const dragStart = () =>{
+const dragStart = (e) =>{
     isDragging = true;
     carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) =>{
     if (!isDragging) return;
-    carousel.scrollLeft = e.pageX
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
 }
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
