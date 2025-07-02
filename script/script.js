c5a1ee53aa40078d2f8cd55b0cdff039dd76fefa
Lucas Figueiredo <91294318+DomLucaz@users.noreply.github.com>
2025-07-02 12:32:30 -0300

    nav bar responsive completed

--- script/script.js
+++ script/script.js
@@ -48,3 +48,19 @@ window.addEventListener("scroll", function (){
     lastScroll = scrollTop;
 });
 
+
+// Show menu mobile
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+        toggle.addEventListener('click', () => {
+            // Add show-menu class to nav menu
+            nav.classList.toggle('show-menu')
+            // Add show-icon to show and hide menu icon
+            toggle.classList.toggle('show-icon') 
+        })
+}   
+
+showMenu('nav-toggle','nav-menu')
